0e594169b31d72a339282e28fc37b8a65c760047
Julien Schueller <schueller@phimeca.com>
2020-05-06 07:51:37 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,7 @@ mkdir build && cd build
 
 cmake -LAH \
   -DCMAKE_PREFIX_PATH="$PREFIX" \
+  -DCMAKE_FIND_FRAMEWORK=LAST \
   -DCMAKE_INSTALL_PREFIX="$PREFIX" \
   -DCMAKE_INSTALL_LIBDIR=lib \
   -DCMAKE_BUILD_TYPE="Release"  \
